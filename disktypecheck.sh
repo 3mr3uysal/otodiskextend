96d577b6694638f9dbd82404a52048e9cb859077
petbardaktaviski <87938245+petbardaktaviski@users.noreply.github.com>
2024-04-16 18:51:55 +0300

    Update disktypecheck.sh

--- disktypecheck.sh
+++ disktypecheck.sh
@@ -5,8 +5,7 @@ df_output=$(df -h)
 if [[ $df_output =~ /dev/mapper ]]; then
     echo "$df_output"
     echo "--LVM DİSK TESPİT EDİLDİ --"
-    echo "LVM icin sanallastirma platformu uzerinden "new device" olarak disk eklemen gerek. eklediysen E tusuna basarak devam edebilir yada hemen ekleyerek ardıdnan E tusuna baa
-sarak devam edebilirsin. iptal icin herhangi bir tusa bas."
+    echo "LVM icin sanallastirma platformu uzerinden "new device" olarak disk eklemen gerek. eklediysen E tusuna basarak devam edebilir yada hemen ekleyerek ardindan E tusuna asarak devam edebilirsin. iptal icin herhangi bir tusa bas."
     read -n 1 input
     if [[ $input == 'e' ]]; then
         echo -n
@@ -17,6 +16,14 @@ sarak devam edebilirsin. iptal icin herhangi bir tusa bas."
 else
     echo "$df_output"
     echo "--STANDART PARTITION TESPIT EDILDI--"
+    echo "Standart Partition icin sanallastirma platformu uzerinden mevcut diskin boyutunu arttirman gerek. arttirdiysan E tusuna basarak devam edebilir yada hemen arttirip ardindan E tusuna basarak devam edebilirsin. iptal icin herhangi bir tusa bas."
+    read -n 1 input
+    if [[ $input == 'e' ]]; then
         echo -n
         ./stddiskextend.sh
+    else
+        echo "byeee"
+    fi
+        echo -n
+
 fi
