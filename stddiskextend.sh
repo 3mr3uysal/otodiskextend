2c1225903bd1531dfca25eaf83f1be2e4679f2f8
petbardaktaviski <87938245+petbardaktaviski@users.noreply.github.com>
2024-04-17 11:05:43 +0300

    Update stddiskextend.sh

--- stddiskextend.sh
+++ stddiskextend.sh
@@ -5,6 +5,7 @@ largest_fs=$(df -h | grep "^/dev/sda" | awk -F"/dev/sda| " '{print $2}' | sort -
 echo 1 > /sys/class/block/sda/device/rescan
 sleep 3
 echo 1 > /sys/class/block/sda/device/rescan
-
+sleep 3
 growpart /dev/sda $largest_fs
+sleep 2
 resize2fs /dev/sda$largest_fs
